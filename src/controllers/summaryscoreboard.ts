c0f05d61f4a6733b2691451469f3f10b7b6d79c9
Osirys25 <mikolajformanski@gmail.com>
2025-04-09 15:52:51 +0200

    Feature: Add Scoreboards with tests

--- src/controllers/summaryscoreboard.ts
+++ src/controllers/summaryscoreboard.ts
@@ -1,11 +1,26 @@
 import {Scoreboard} from './scoreboard.ts';
 import {Match} from '../models/match.ts';
 
+/**
+ * The SummaryScoreboard class extends the Scoreboard class, adding functionality
+ * to manage and sort matches based on their scores.
+ */
 export class SummaryScoreboard extends Scoreboard {
+    /**
+     * Adds a new match to the scoreboard.
+     * @param {Match} match - The match object to add.
+     * @returns {void}
+     */
     addMatch(match: Match): void {
         this.table.push(match);
     }
 
+    /**
+     * Returns a sorted array of matches based on their scores.
+     * Matches are sorted in descending order by the total score of both teams.
+     * In case of a tie, matches are sorted by start time (newest first).
+     * @returns {Match[]} - The sorted array of matches.
+     */
     getScoreboard(): Match[] {
         return this.table.sort((a: Match, b: Match) => {
             const totalScoreA: number = a.homeTeam.score + a.awayTeam.score;
